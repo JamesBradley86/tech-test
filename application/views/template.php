b9ceb9200bc820e9abfb7ad2137ebbafaec5adfb
Jamie Bradley <evilunix@splooshmedia.co.uk>
2016-01-28 14:21:52 +0000

    Added template view file

---
+++ application/views/template.php
@@ -0,0 +1,47 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Sky Betting &amp; Gaming Technical Test</title>
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <form>
+        <table>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+            </tr>
+            <tr>
+                <td><input type="text" name="people[][firstname]" value="Jeff" /></td>
+                <td><input type="text" name="people[][surname]" value="Stelling" /></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="people[][firstname]" value="Chris" /></td>
+                <td><input type="text" name="people[][surname]" value="Kamara" /></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="people[][firstname]" value="Alex" /></td>
+                <td><input type="text" name="people[][surname]" value="Hammond" /></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="people[][firstname]" value="Jim" /></td>
+                <td><input type="text" name="people[][surname]" value="White" /></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="people[][firstname]" value="Natalie" /></td>
+                <td><input type="text" name="people[][surname]" value="Sawyer" /></td>
+            </tr>
+        </table>
+        <input type="submit" value="OK" />
+    </form>
+</body>
+
+</html>
